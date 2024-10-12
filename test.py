b2cecf11d5fcd86f89bfe1c42a257d2efd906f97
RealAriya <arya.t.innovate@gmail.com>
2024-10-12 22:05:18 +0330

    even or odd
    improve to recognize that the random number is even or odd

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 import random
 
-print(random.randint(1,100))
+x = random.randint(1,100)
+
+if x%2 == 0:
+    print(f"{x} is even.")
+elif x == 0:
+    print("Error")
+else:
+    print(f"{x} is odd.")
+
 
