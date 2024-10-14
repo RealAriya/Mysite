a6764e816abd69369c6c840abe048b295631fc5b
RealAriya <arya.t.innovate@gmail.com>
2024-10-14 18:09:09 +0330

    Create views
    Define our function that we need to address in urls.py file here

---
+++ mysite/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse,JsonResponse
+
+
+def http_test(request):
+    return HttpResponse('hello')
+
+def json_test(request):
+    return JsonResponse({'name': 'Arya', 'family-name':'Tahmasebi'})
