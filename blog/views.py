c04aa63e77eaeecfac3d104fb2a06fc425930135
RealAriya <arya.t.innovate@gmail.com>
2024-10-22 20:38:13 +0330

    create context
    + fetch data from dictionary in views.py

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 
 def blog_home(request):
-    return render(request,'blog/blog-home.html')
+    context = {'post_1':'Bitcoin crashed', 'post_2':'Gold rise up', 'post_3':'Barca win all games','post_4':'Real Madrid vs Dortmond'}
+    return render(request,'blog/blog-home.html',context)
 
 def blog_single(request):
+    
     return render(request,'blog/blog-single.html')
+
