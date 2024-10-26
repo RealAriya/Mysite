52f847ec65ade5a6fd7ba546b62dfaf401abafea
RealAriya <arya.t.innovate@gmail.com>
2024-10-26 19:24:06 +0330

    To show other column in post admin page

--- blog/admin.py
+++ blog/admin.py
@@ -6,5 +6,6 @@ from .models import post
 class postAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = "-empty-"
+    list_display = ['title','counted_view','status','published_date','created_date']
 
 # admin.site.register(post,postAdmin)
