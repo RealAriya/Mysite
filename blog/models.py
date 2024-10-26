00bf6aeacbbfa207f0e4ac2dbfe3b5a6313d76db
RealAriya <arya.t.innovate@gmail.com>
2024-10-26 18:46:49 +0330

    create admin.py
    + import from blog.models post
    + admin.site.register(Author, AuthorAdmin)
    + now we can see all these in graphic version

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,7 @@ class post(models.Model):
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):          # To see id numbers at the same time 
+        return self.title       # return " {} - {} ".format(self.title,self.id)
