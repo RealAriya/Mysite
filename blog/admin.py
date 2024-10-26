777499e46beed52fa9ab77754bd0bea46324ef47
RealAriya <arya.t.innovate@gmail.com>
2024-10-26 19:29:50 +0330

    Open a filter panel in page in base of ( '', )

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,5 @@ class postAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = "-empty-"
     list_display = ['title','counted_view','status','published_date','created_date']
-
+    list_filter = ('status','counted_view')
 # admin.site.register(post,postAdmin)
