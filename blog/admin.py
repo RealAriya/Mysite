d56e7b43bbfd9bf52734e8853e0a1f91205780fd
RealAriya <arya.t.innovate@gmail.com>
2024-11-02 19:07:27 +0330

    Add author as foreign key
    + from django.contrib.auth.models import User
    + we can either CASCADE (this will delete the whole post) or SET_NULL (this just put nothing in name of author.)

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from blog.models import post
 class postAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = "-empty-"
-    list_display = ['title','counted_view','status','published_date','created_date']
+    list_display = ['title','author','counted_view','status','published_date','created_date']
     list_filter = ('status','counted_view')
     # ordering = ["created_date"]  # we can put mines behind the name to get reverse output ["-created_date"]
     search_fields = ["title",'content']
