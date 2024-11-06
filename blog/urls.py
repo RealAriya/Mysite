8331c3bab09439bb53dbecebd27c4fa54406c385
RealAriya <arya.t.innovate@gmail.com>
2024-11-06 23:40:26 +0330

    create a path that show us specific authors
    + create path in urls.py
    + create function in view that call all posts then
        filter them by category
    + author__username we write this cuz in
        database
    +   author known by foreign  id's so we tell go to
          id's and say give me their usernames

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', blog_home, name='home'),
     path('<int:pid>', blog_single, name='single'),
     path('test',test,name = 'test'),
-    path('category/<str:cat_name>', blog_home, name='category')
-
+    path('category/<str:cat_name>', blog_home, name='category'),
+    path('author/<str:author_username>/',blog_home,name='author')
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,13 @@ from django.utils import timezone
 
 
 
-def blog_home(request,cat_name=None):
+def blog_home(request,**kwargs):
     now = timezone.now()
     posts = post.objects.filter(status = 1 , published_date__lte=now)
-    if cat_name:
-        posts = posts.filter(category__name=cat_name)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts': posts}
     return render(request,'blog/blog-home.html',context)
 
