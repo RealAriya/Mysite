f1c6c05f6fc41e43556f877fd217cff8932c3ffb
RealAriya <arya.t.innovate@gmail.com>
2024-11-12 15:49:35 +0330

    Django extension
    + It's not much productive in windows( It's use in
        Linux)
    + just help you to know your whole project by
       graph or recognize a bug etc.

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.humanize',
+    'django_extensions',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
     'blog'
