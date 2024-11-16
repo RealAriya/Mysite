80afc4f4276219a6bd4a71f08797557ff5b8c64a
RealAriya <arya.t.innovate@gmail.com>
2024-11-16 18:02:51 +0330

    How to check authentication through template
    + Just write it down on your html page

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
 
 def login_view(request):
-    if request.user.is_authenticated:
-        message = f' User is authenticated as {request.user.username}'
-    else:
-        message = f' User is not authenticated as {request.user.username}'
-    
-    context={
-        'message':message
-    }
-    return render(request,'accounts/login.html',context)
+    return render(request,'accounts/login.html')
 
 # def logout_view(request):
 #     pass
