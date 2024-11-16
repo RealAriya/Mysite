d0ff3cff92886659353351e8decd5365516cf6e4
RealAriya <arya.t.innovate@gmail.com>
2024-11-16 21:38:17 +0330

    How create login form by built in forms in django
    https://docs.djangoproject.com/en/4.2/topics/auth/default/
    + In this page search : django.contrib.auth.forms
    + read documentation
    + this way has more advantages cuz we check
        with is_valid
    + also check if user already logged in the login
       page won't open (first if in view)

--- accounts/views.py
+++ accounts/views.py
@@ -2,19 +2,30 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.add_message(request, messages.SUCCESS, "Your logged in successfully.")
-            return redirect('/')
-        
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request,data=request.POST)
+            if form.is_valid():
+                username = request.POST["username"]
+                password = request.POST["password"]
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.add_message(request, messages.SUCCESS, "Your logged in successfully.")
+                    return redirect('/')
+            
+        form = AuthenticationForm() 
+        context = {
+            'form': form
+        }   
     
-    return render(request,'accounts/login.html')
+        return render(request,'accounts/login.html',context)
+    else:
+        messages.add_message(request, messages.INFO, "You already logged in..")
+        return redirect('/')
 
 # def logout_view(request):
 #     pass
