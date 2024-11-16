7516b22433181c06ba2fec5e08c017e6708846e7
RealAriya <arya.t.innovate@gmail.com>
2024-11-16 21:11:47 +0330

    create login form through view
    + download a login template
    + import it's js code
    + control it in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib import messages
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.add_message(request, messages.SUCCESS, "Your logged in successfully.")
+            return redirect('/')
+        
+    
     return render(request,'accounts/login.html')
 
 # def logout_view(request):
