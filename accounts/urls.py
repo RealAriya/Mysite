612998787b651e3be7d9385332cd75004be73857
RealAriya <arya.t.innovate@gmail.com>
2024-11-26 14:51:37 +0330

    Auth reset password (finally)

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,18 +17,6 @@ urlpatterns = [
 
     path('reset-password/',
           auth_views.PasswordResetView.as_view(
-          template_name="registration/password_reset_email.html"), 
+          template_name="registration/password_reset_form.html"), 
           name='password_reset'),
-    path('reset-password-sent/',
-          auth_views.PasswordResetDoneView.as_view(
-          template_name="registration/password-sent.html"),
-          name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-          auth_views.PasswordResetConfirmView.as_view(
-          template_name="registration/password_change_form.html"), 
-          name='password_reset_confirm'),
-    path('reset-password-complete/',
-          auth_views.PasswordResetCompleteView.as_view(
-          template_name="registration/password_reset_complete.html"), 
-          name='password_reset_complete'),
 ]
