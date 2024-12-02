4e1b044361f37057ebf6a02278c6e0d67592c06c
RealAriya <arya.t.innovate@gmail.com>
2024-12-02 21:04:54 +0330

    Hosting 1
     change database + update requirement

--- mysite/settings.py
+++ mysite/settings.py
@@ -95,12 +95,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'mysite',
+                'USER': 'aria',
+                'PASSWORD': '763099',
+                'HOST': '127.0.0.1',
+                'PORT': '5432',
+            }
+    }
 
 
 # Password validation
